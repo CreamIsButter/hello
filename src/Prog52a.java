3d35b8026478e3b7f03af717d1d1e68715059ab3
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-09-08 09:47:04 -0500

    Update

---
+++ src/Prog52a.java
@@ -0,0 +1,16 @@
+public class Prog52a {
+    public static void main(String[] args) {
+        int width = 82;
+        int length = 143;
+
+        int area = length * width;
+        int perimeter = length*2 + width*2;
+
+        System.out.println("The area of the rectangle is: " + area);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+}
+/*
+Area = 11726
+Perimeter = 450
+ */
