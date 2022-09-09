ac3de3de6961ad4a7e7a12a4ba1ab38c58f084c3
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-09-09 10:22:27 -0500

    Update

---
+++ src/Prog54c.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Prog54c {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("What is the radius of the circle: ");
+        double radius = keyboard.nextDouble();
+
+        double pi = 3.14159;
+
+        double circumference =
+
+        double areaC =
+
+
+}
