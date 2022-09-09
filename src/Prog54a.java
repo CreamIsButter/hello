f0251b10f898425e48ebbb8b37e58f2bbbc20b4a
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-09-09 09:56:22 -0500

    Update

---
+++ src/Prog54a.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Prog54a {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("What is the model of the car: ");
+        String model = keyboard.nextLine();
+
+        System.out.print("How many miles were driven?: ");
+        double miles = keyboard.nextDouble();
+        System.out.print("How many gallons were used?: ");
+        double gallons = keyboard.nextDouble();
+
+        double mpg = miles/gallons;
+        System.out.println("The average MPG used by the " + model + " are " + mpg + " mpg");
+    }
+}
