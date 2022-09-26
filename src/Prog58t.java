1c7bfc3ca201a60ae98341f0944e2d1fdfde6c8d
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-09-26 10:30:54 -0500

    Update

---
+++ src/Prog58t.java
@@ -0,0 +1,10 @@
+import java.util.*
+
+public class Prog58t {
+    public static void main(String[] args) {
+        Scanner new = new Scanner(System.in);
+
+        System.out.print();
+        double pp =
+    }
+}
