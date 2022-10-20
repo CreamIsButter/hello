6e06f0cc5ac10264a382dcc94f6d0eeacf302b48
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-10-20 10:30:42 -0500

    Update

---
+++ src/Prog122a.java
@@ -0,0 +1,9 @@
+// Brody Vandiver
+// Program 122a
+// 10/20/22
+public class Prog122a {
+    public static void main(String[] args){
+        System.out.printf("Number\t\tSquare\t\t\tSquare Root");
+    }
+
+}
