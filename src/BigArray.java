0f53e82dae50bb4809e2ad44ba5f56aa2bcc668b
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-11-16 10:47:30 -0600

    Update

---
+++ src/BigArray.java
@@ -0,0 +1,169 @@
+public class BigArray {
+    public static void main(String[] args) {
+        int[] array = new int[19];
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            array[lcv] = (int) (Math.random() * (91 - 20) + 20);
+        }
+
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            System.out.print(array[lcv] + " ");
+        }
+        System.out.println();
+
+        for (int temp : array) {
+            System.out.print(temp + " ");
+        }
+        System.out.println();
+
+        System.out.println("The middle number is " + array[array.length / 2]);
+
+        double avg = array[0] + array[array.length - 1] + array[array.length / 2];
+        System.out.println("Average of the first, middle, and last numbers is " + (avg/3));
+
+        int smallest = array[0];
+        int largest = array[0];
+        for (int num : array) {
+            if (num < smallest)
+                smallest = num;
+            if (num > largest)
+                largest = num;
+        }
+        System.out.println("The smaller number is: " + smallest);
+        System.out.println("The largest number is: " + largest);
+
+        for (int lcv = 0; lcv < array.length; lcv++){
+            if (array[lcv] == smallest)
+                array[lcv] = largest;
+            else if (array[lcv] == largest)
+                array[lcv] = smallest;
+        }
+
+        for (int x : array) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        int randNum = (int)(Math.random() * 10 + 1);
+        array[array.length / 2] = randNum;
+
+        for (int x : array)
+            System.out.print(x + " ");
+        System.out.println();
+
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            array[lcv] += 10;
+        }
+
+        for (int x : array) System.out.print(x + " ");
+        System.out.println();
+
+
+
+        int myThird = array[2];
+        array[2] = 5;
+        System.out.println("The number that was ousted is " + myThird);
+
+
+
+        for (int num : array) {
+            if (num >= 50 && num <= 59)
+                System.out.println(num + " ");
+        }
+        System.out.println();
+
+
+
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            if (array[lcv]%4==0) {
+                System.out.println(array[lcv] + " ");
+            }
+        }
+        System.out.println();
+
+
+
+        boolean sixty = false;
+        for (int temp : array) {
+            if (temp == 60)
+                sixty = true;
+        }
+        System.out.println("Is sixty in the list? " + sixty);
+
+
+
+        boolean same = true;
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            if (array[lcv] != array[array.length - 1 - lcv])
+                same = false;
+        }
+        System.out.println("Is the array palindromic? " + same);
+
+
+
+        double average = 0;
+        for (int x : array)
+            average += x;
+        average /= array.length;
+
+        int count = 0;
+        for (int x : array) {
+            if ( int x > average){
+                count++;
+            }
+        }
+        System.out.printf("There are %d numbers greater than the average\n", count);
+
+
+
+        int evens = 0;
+        for (int num : array)
+            if(num%2 == 0)
+                evens++;
+            System.out.printf("There are %d even numbers\n", evens);
+
+
+
+            int[] array2 = new int[array.length];
+            for (int lcv = 0; lcv < array.length; lcv++) {
+                array2[lcv] = array[array.length - 1 - lcv];
+            }
+
+            for (int n : array2)
+                System.out.print(n + " ");
+            System.out.println();
+
+
+
+            int[] shiftArray = circularShiftRight(array);
+            for (int num : shiftArray)
+                System.out.print(num + " ");
+            System.out.println();
+
+
+
+            int sum = 0;
+            for (int num : array) {
+                int digitsum = 0;
+                int temp = sum;
+                while (temp > 0) {
+                    int n = temp % 10;
+                    temp /= 10;
+                    digitsum += n;
+                }
+                sum += digitsum;
+            }
+            System.out.println("Sum of all elements");
+    }
+
+    public static int[] circularShiftRight(int[] arr) {
+        int temp = arr[arr.length - 1];
+        int[] shifted = new int[arr.length];
+        for (int lcv = 0; lcv < shifted.length; lcv++)
+            shifted[lcv] = arr[lcv];
+
+        for (int lcv = 0; lcv < shifted.length - 1; lcv++)
+
+        shifted[0] = temp;
+        return shifted;
+    }
+}
---
+++ src/BigArray2.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BigArray2 {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/bigarraylist.dat"));
+            int numCats = input.nextInt();
+            input.nextLine();
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+    }
+}
