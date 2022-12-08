a829b5f23498b97f65d1bd4958c9858598c3f6a6
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-12-08 10:29:18 -0600

    Update

--- src/LP55.java
+++ src/LP55.java
@@ -6,6 +6,30 @@ import java.util.*;
 
 public class LP55 {
     public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
+    System.out.print("Enter a positive integer: ");
+    String intgr = input.nextLine();
+
+    for (int lcv = 0; lcv < intgr.length(); lcv++) {
+        String inty = intgr.substring(lcv, lcv + 1);
+        System.out.println(inty);
+
+        }
     }
+
 }
+
+/*
+Enter a positive integer: 2183763981
+2
+1
+8
+3
+7
+6
+3
+9
+8
+1
+ */
