3dac3118c6a479de89c3790514f056c500415f0d
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-12-15 10:26:45 -0600

    Update

--- src/LP59.java
+++ src/LP59.java
@@ -1,9 +1,28 @@
-import java.util.*;
-
 // Brody Vandiver
-// 12/09/22
-// LP 5 Exercise 5
+// 12/15/22
+// LP 5 Exercise 9
 
 public class LP59 {
+    public static void main(String[] args) {
+        System.out.println("x^1\t\t\tx^2\t\t\tx^3\t\t\tx^4\t\t\tx^5");
+        for (int lcv = 1; lcv <= 6; lcv++){
+            for (int lcv2 = 1; lcv2 <= 5; lcv2++) {
+                int num = (int) Math.pow(lcv, lcv2);
+                System.out.print(num + "\t\t\t");
+            }
 
+            System.out.println();
+
+        }
+    }
 }
+
+/*
+x^1			x^2			x^3			x^4			x^5
+1			1			1			1			1
+2			4			8			16			32
+3			9			27			81			243
+4			16			64			256			1024
+5			25			125			625			3125
+6			36			216			1296		7776
+ */
