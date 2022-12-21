91dc9f7c7b3aed32f2ae90aa10e98b7eb0de5e6c
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2022-12-21 10:48:10 -0600

    Update

--- src/MSOE20161.java
+++ src/MSOE20161.java
@@ -6,10 +6,17 @@ public class MSOE20161 {
         System.out.println("Enter a word in lowercase: ");
         String pali = input.nextLine();
 
+        int x = 0;
+
         for (int lcv = 0; lcv < pali.length(); lcv++) {
+            System.out.println(pali.substring(lcv) + pali.substring(pali.length() - lcv));
+            if (pali.substring(lcv, lcv) != pali.substring(pali.length() - lcv, pali.length() - lcv)) {
+                x += 1;
+            }
+            if (x == pali.length())
+                System.out.print(x + "" + pali.length());
+
 
         }
-
-
     }
 }
