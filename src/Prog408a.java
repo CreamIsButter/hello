1abd9e65e2ca729ebce141960d8af0cf1f31fc59
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2023-02-14 10:12:42 -0600

    Update

---
+++ src/Prog408a.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Prog408a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/prog408a.dat"));
+            Cl408[] list = new Cl408[21];
+            int count = 0;
+            while (input.hasNext()) {
+                int id = input.nextInt();
+                int score = input.nextInt();
+                list[count] = new Cl408a(id, score);
+                count++;
+            }
+
+            list = SortingAlgs.insertionSort(list);
+            System.out.println("ID\tScpre");
+            for (int lcv = 0; lcv < list.length; lcv;)
+                System.out.println(list[lcv].getID() + "\t" list[lcv].getScore());
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
---
+++ src/SortingAlgs.java
@@ -0,0 +1,58 @@
+public class SortingAlgs {
+
+    public static int[] bubbleSort(int[] array) {
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            for (int lcv2 = 0; lcv2 < array.length-1; lcv2++) {
+                if (array[lcv2] > array[lcv2 +1]) {
+                    int temp = array[lcv2];
+                    array[lcv2] = array[lcv2 + 1];
+                    array[lcv2 + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] selectionSort(int[] array) {
+        for (int lcv = 0; lcv < array.length; lcv++) {
+            int min = lcv;
+            for (int lcv2 = lcv + 1; lcv2 < array.length; lcv2++) {
+                if (array[lcv2] < array[min]) min = lcv2;
+                {
+                    int temp = array[lcv];
+                    array[lcv] = array[min];
+                    array[min] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] insertionSort(int[] array) {
+        for (int lcv = 1; lcv < array.length; lcv++) {
+            int temp = array[lcv];
+            int lcv2 = lcv - 1;
+            while (lcv2 >= 0 && array[lcv2] > temp) {
+                array[lcv2+1] = array[lcv2];
+                lcv2--;
+            }
+            array[lcv2+1] = temp;
+        }
+        return array;
+    }
+
+    public static Cl408a
+    public static int[] insertionSort(int[] array) {
+        for (int lcv = 1; lcv < array.length; lcv++) {
+            int temp = array[lcv];
+            int lcv2 = lcv - 1;
+            while (lcv2 >= 0 && array[lcv2] > temp) {
+                array[lcv2+1] = array[lcv2];
+                lcv2--;
+            }
+            array[lcv2+1] = temp;
+        }
+        return array;
+    }
+}
+
