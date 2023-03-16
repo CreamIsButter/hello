7e3c4d0372271fbdb4914641d39608ef4f1f5b24
CreamIsButter <vandiver.b@stu.janesville.k12.wi.us>
2023-03-16 10:28:51 -0500

    Update

--- src/Prog607a.java
+++ src/Prog607a.java
@@ -7,9 +7,28 @@ public class Prog607a {
         System.out.println("0) Quit");
         System.out.println("1) Encode Date");
         System.out.println("2) Decode Date");
+
+        String[] array1 = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+        String[] array2 = {"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] array3 = {"Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"};
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Select an option: ");
+
+        System.out.print("Select an option: ");
         int opt = input.nextInt();
+
+        if (opt == 1){
+        System.out.print("enter date (m/d/y) ");
+        String date = input.next();
+
+        for (int lcv = 0; lcv < date.length(); lcv++) {
+            while (date.substring(lcv) != "/") {
+                System.out.print(array1.)
+
+            }
+        }
+        }
 
     }
 }
